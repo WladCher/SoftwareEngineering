e0e27cbf27b290a717ac6000dc47c4b1224a96eb
WladCher <71927380+WladCher@users.noreply.github.com>
2025-09-29 03:54:25 +0500

    Add files via upload

---
+++ py/samrab3.py
@@ -0,0 +1,10 @@
+x = int(input("Введите число 0-10: "))
+if 0 <= x <= 10:
+    if x <= 3:
+        print("0–3")
+    elif x <= 6:
+        print("3–6")
+    else:
+        print("6–10")
+else:
+    print("Число вне диапазона")
---
+++ py/samrab4.py
@@ -0,0 +1,7 @@
+s = input("Введите предложение: ")
+print("Длина:", len(s))
+print("Нижнем регистр:", s.lower())
+print("Количество гласных:", sum(ch in "aeiou" for ch in s.lower()))
+print("Замена ugly:", s.replace("ugly", "beauty"))
+print("Начинается с 'The':", s.startswith("The"))
+print("Заканчивается на 'end':", s.endswith("end"))
